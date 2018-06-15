58bef353fc3e42970afb9a67beda672b8463c19d
jerry2605 <jerry2605@hotmail.com>
2018-06-15 12:09:21 +0800

    Update aws-cost-and-usage-report.py

--- aws-cost-and-usage-report.py
+++ aws-cost-and-usage-report.py
@@ -3,6 +3,8 @@
 import argparse
 import boto3
 import datetime
+import os
+import csv
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--days', type=int, default=30)
@@ -13,7 +15,7 @@ now = datetime.datetime.utcnow()
 start = (now - datetime.timedelta(days=args.days)).strftime('%Y-%m-%d')
 end = now.strftime('%Y-%m-%d')
 
-cd = boto3.client('ce', 'us-east-1')
+cd = boto3.client('ce', 'us-west-1')
 
 results = []
 
@@ -29,9 +31,24 @@ while True:
     if not token:
         break
 
-print('\t'.join(['TimePeriod', 'LinkedAccount', 'Service', 'Amount', 'Unit', 'Estimated']))
-for result_by_time in results:
-    for group in result_by_time['Groups']:
-        amount = group['Metrics']['UnblendedCost']['Amount']
-        unit = group['Metrics']['UnblendedCost']['Unit']
-        print(result_by_time['TimePeriod']['Start'], '\t', '\t'.join(group['Keys']), '\t', amount, '\t', unit, '\t', result_by_time['Estimated'])
+path = os.getcwd()
+os.makedirs(path+"//csv//", exist_ok=True)
+with open(path + "//csv//" + 'results.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+#saveFile = open(path + "//csv//" + 'results.csv','w', encoding='utf-8')
+
+    print('\t'.join(['TimePeriod', 'LinkedAccount', 'Service', 'Amount', 'Unit', 'Estimated']))
+    #saveFile.write('\t'.join(['TimePeriod', 'LinkedAccount', 'Service', 'Amount', 'Unit', 'Estimated'])+'\n')
+    writer.writerow(['TimePeriod', 'LinkedAccount', 'Service', 'Amount', 'Unit', 'Estimated'])
+    
+    for result_by_time in results:
+        for group in result_by_time['Groups']:
+            amount = group['Metrics']['UnblendedCost']['Amount']
+            unit = group['Metrics']['UnblendedCost']['Unit']
+            print(result_by_time['TimePeriod']['Start'], '\t', '\t'.join(group['Keys']), '\t', amount, '\t', unit, '\t', result_by_time['Estimated'])
+            record = result_by_time['TimePeriod']['Start'], group['Keys'][0],group['Keys'][1],  amount,  unit,  str(result_by_time['Estimated'])
+            #for e in record:
+               # saveFile.write(e)
+            writer.writerow(record)
+    
+    print("results.csv is saved")
